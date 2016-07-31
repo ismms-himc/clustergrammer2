c33471a5b1681ad5a2a3b9caff1e9c277c7d73cf
cornhundred <doctorcorn2002@gmail.com>
2016-07-31 17:23:17 -0400

    calculating viz json, working on passing it to front-end and getting visualization

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -1,7 +1,7 @@
 import ipywidgets as widgets
 import json
 from traitlets import Unicode
-
+from clustergrammer import Network
 
 @widgets.register('hello.Hello')
 class HelloWorld(widgets.DOMWidget):
@@ -12,7 +12,23 @@ class HelloWorld(widgets.DOMWidget):
     _model_module = Unicode('clustergrammer_widget').tag(sync=True)
     value = Unicode('updating python by restarting kernel').tag(sync=True)
 
+    print('\n\ninitializing net\n-----------------\n\n')
+    net = Network()
+    print('loaded file')
+
+    net.load_file('rc_two_cats.txt')
+
+    net.make_clust()
+
+    # viz_json = net.export_net_json()
+
+    print(net.viz.keys())
+
+
+
     #
     network = {'nodes':'nodes', 'links':'links'}
     network_string = json.dumps(network)
     network = Unicode(network_string).tag(sync=True)
+
+
