ffd5393ffe080de9fb65a95fa26f2d8b816d8bb3
cornhundred <doctorcorn2002@gmail.com>
2016-08-01 11:11:55 -0400

    working loading externally data

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -12,17 +12,10 @@ class HelloWorld(widgets.DOMWidget):
     _model_module = Unicode('clustergrammer_widget').tag(sync=True)
     value = Unicode('updating python by restarting kernel').tag(sync=True)
 
-    print('\n\ninitializing net\n-----------------\n\n')
-    net = Network()
-    print('loaded file')
+    # net = Network()
+    # net.load_file('rc_two_cats.txt')
+    # net.make_clust()
+    # network = net.viz
 
-    net.load_file('rc_two_cats.txt')
-
-    net.make_clust()
-
-    print(net.viz.keys())
-
-    network = net.viz
-
-    network_string = json.dumps(network)
-    network = Unicode(network_string).tag(sync=True)
+    # network_string = json.dumps(network)
+    network = Unicode('').tag(sync=True)
