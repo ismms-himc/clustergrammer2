15dafe7d36bccbb7c1e3edb519a11773bbd1dda8
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-03-29 12:49:14 -0400

    passing arguments from cgm_widget.cluster to net.make_clust

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -24,10 +24,22 @@ class clustergrammer_widget(widgets.DOMWidget, Network):
     def widget_df(self):
       print('exporting current dataframe')
 
-    def cluster(self):
+    def cluster(self, dist_type='cosine', run_clustering=True,
+                 dendro=True, views=['N_row_sum', 'N_row_var'],
+                 linkage_type='average', sim_mat=False, filter_sim=0.1,
+                 calc_cat_pval=False, run_enrichr=None):
       '''
       Cluster the matrix and assign the visualization JSON to network for front
       end rendering.
       '''
-      self.make_clust()
+      self.make_clust(dist_type=dist_type, run_clustering=run_clustering,
+                                   dendro=dendro,
+                                   views=views,
+                                   linkage_type=linkage_type,
+                                   sim_mat=sim_mat,
+                                   filter_sim=filter_sim,
+                                   calc_cat_pval=calc_cat_pval,
+                                   run_enrichr=run_enrichr)
+
+      # pass visualization JSON to widget
       self.network = self.widget()
