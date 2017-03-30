bfb38943889730546a04975580d22371ebde94ad
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-03-30 13:39:34 -0400

    removed methods from clustergrammer_widget, this no longer inherits from Network

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -20,31 +20,3 @@ class clustergrammer_widget(widgets.DOMWidget):
     viz_title = Unicode('updating python by restarting kernel').tag(sync=True)
 
     network = Unicode('').tag(sync=True)
-
-    # def widget_df(self):
-    #   df_string = self.mat_string
-
-    #   self.load_file_as_string(df_string)
-    #   df = self.export_df()
-
-    #   return df
-
-    # def cluster(self, dist_type='cosine', run_clustering=True,
-    #              dendro=True, views=['N_row_sum', 'N_row_var'],
-    #              linkage_type='average', sim_mat=False, filter_sim=0.1,
-    #              calc_cat_pval=False, run_enrichr=None):
-    #   '''
-    #   Cluster the matrix and assign the visualization JSON to network for front
-    #   end rendering.
-    #   '''
-    #   self.make_clust(dist_type=dist_type, run_clustering=run_clustering,
-    #                                dendro=dendro,
-    #                                views=views,
-    #                                linkage_type=linkage_type,
-    #                                sim_mat=sim_mat,
-    #                                filter_sim=filter_sim,
-    #                                calc_cat_pval=calc_cat_pval,
-    #                                run_enrichr=run_enrichr)
-
-    #   # pass visualization JSON to widget
-    #   self.network = self.widget()
