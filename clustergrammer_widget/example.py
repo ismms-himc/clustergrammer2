b5bfa5ebfe1a151ac4559c4ab020dd18f1822638
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-04-05 12:56:34 -0400

    version 1.9.2 with hzome request aborting after 1 second

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -5,7 +5,7 @@ from traitlets import Unicode
 from . import clustergrammer
 Network = clustergrammer.Network
 
-# version 1.9.1
+# version 1.9.2
 
 @widgets.register('hello.Hello')
 class clustergrammer_widget(widgets.DOMWidget):
