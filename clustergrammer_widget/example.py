43f9e50aa958f2042359aeb6918a366f7475d9a5
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-04-06 14:53:12 -0400

    version 1.9.9 prevent update_matrix_string in nbviewer

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -5,7 +5,7 @@ from traitlets import Unicode
 from . import clustergrammer
 Network = clustergrammer.Network
 
-# version 1.9.8
+# version 1.9.9
 
 @widgets.register('hello.Hello')
 class clustergrammer_widget(widgets.DOMWidget):
