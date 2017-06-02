2091a05be1dc09f9d1bb73f02bfd80a243e3a90f
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-06-02 17:54:23 -0400

    version 1.10.7 with small bug fixes for enrichrgram

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -5,7 +5,7 @@ from traitlets import Unicode
 from . import clustergrammer
 Network = clustergrammer.Network
 
-# version 1.10.6
+# version 1.10.7
 
 @widgets.register('hello.Hello')
 class clustergrammer_widget(widgets.DOMWidget):
