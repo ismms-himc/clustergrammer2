444b2e5542ce8744363bbe041e039222ca1d3df5
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-06-06 12:34:47 -0400

    version 1.11.1 cat-breakdown tooltip now has pct

--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -5,7 +5,7 @@ from traitlets import Unicode
 from . import clustergrammer
 Network = clustergrammer.Network
 
-# version 1.11.0
+# version 1.11.1
 
 @widgets.register('hello.Hello')
 class clustergrammer_widget(widgets.DOMWidget):
--- js/src/example.js
+++ js/src/example.js
@@ -6,7 +6,7 @@ var ini_hzome = require('./hzome_functions');
 var Enrichrgram = require('./Enrichrgram');
 var url = require("file-loader!./clustergrammer_logo.png");
 
-// version 1.11.0
+// version 1.11.1
 
 require('!style!css!./custom.css');
 
