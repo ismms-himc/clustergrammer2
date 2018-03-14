3da9168500c39d17dd9a2ec74de7d856133e5913
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-03-14 17:16:18 -0400

    1.13.3, fixed rendering of unclustered heatmap

--- clustergrammer_widget/_version.py
+++ clustergrammer_widget/_version.py
@@ -1,2 +1,2 @@
-version_info = (1, 13, 2)
+version_info = (1, 13, 3)
 __version__ = '.'.join(map(str, version_info))
--- clustergrammer_widget/example.py
+++ clustergrammer_widget/example.py
@@ -5,7 +5,7 @@ from traitlets import Unicode
 from . import clustergrammer
 Network = clustergrammer.Network
 
-# version 1.13.2
+# version 1.13.3
 
 @widgets.register('hello.Hello')
 class clustergrammer_widget(widgets.DOMWidget):
