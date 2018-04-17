3d86babd1045343e180e0baa1435e7a047366b2e
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-04-17 15:35:49 -0400

    clustergrammer_glidget version 0.2.3 improved default cat color white

--- clustergrammer_glidget/example.py
+++ clustergrammer_glidget/example.py
@@ -5,7 +5,7 @@ from traitlets import Unicode
 from . import clustergrammer
 Network = clustergrammer.Network
 
-# version 0.2.2
+# version 0.2.3
 
 @widgets.register('hello.Hello')
 class clustergrammer_glidget(widgets.DOMWidget):
