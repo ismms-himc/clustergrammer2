11cefe91294e0f4eb58c85e4b1d03937c8ccac7b
cornhundred <nickfloresfernandez@gmail.com>
2018-11-04 18:22:52 -0500

    working on releasing 0.2.5 (testing relesse cycle under new repo)

--- clustergrammer2/_version.py
+++ clustergrammer2/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Nicolas Fernandez.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 4)
+version_info = (0, 2, 5)
 __version__ = ".".join(map(str, version_info))
