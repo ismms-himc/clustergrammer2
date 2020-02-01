b8bd3775e66928928b2e4330132daaeb4f1d88e2
cornhundred <nickfloresfernandez@gmail.com>
2020-02-01 09:18:00 -0500

    version 0.5.8 bug fix for value based opacity and 200 labels

--- src/widget.ts
+++ src/widget.ts
@@ -14,7 +14,7 @@ import cgm_fun from 'clustergrammer-gl';
 import * as d3 from 'd3';
 
 console.log('********************************************')
-console.log('** clustergrammer2 frontend version 0.5.7 **')
+console.log('** clustergrammer2 frontend version 0.5.8 **')
 console.log('********************************************')
 console.log('working on traitlets for Voila')
 
