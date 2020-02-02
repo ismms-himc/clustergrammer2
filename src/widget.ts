4d74fd029bb3790f6ef8b57a96ab5ed6a0e99f3e
cornhundred <nickfloresfernandez@gmail.com>
2020-02-02 12:44:44 -0500

    version 0.5.14 syncing dendrogram indices

--- src/widget.ts
+++ src/widget.ts
@@ -14,7 +14,7 @@ import cgm_fun from 'clustergrammer-gl';
 import * as d3 from 'd3';
 
 console.log('********************************************')
-console.log('** clustergrammer2 frontend version 0.5.13 **')
+console.log('** clustergrammer2 frontend version 0.5.14 **')
 console.log('********************************************')
 
 export
