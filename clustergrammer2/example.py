b7ac49924b5171a2b5f2ccab4fc501d994c0ab3c
cornhundred <nickfloresfernandez@gmail.com>
2020-05-06 13:39:51 -0400

    version 0.5.22 Jlab backward compatibility

--- clustergrammer2/example.py
+++ clustergrammer2/example.py
@@ -27,6 +27,6 @@ class ExampleWidget(DOMWidget):
 
     mat_string = Unicode('ini_mat_string').tag(sync=True)
 
-    print('>> clustergrammer2 backend version 0.5.21')
+    print('>> clustergrammer2 backend version 0.5.22')
 
     network = Unicode('').tag(sync=True)
