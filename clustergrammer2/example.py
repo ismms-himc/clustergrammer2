340bd8e3fa134082c585589f0d4343f91c8e9c85
cornhundred <nickfloresfernandez@gmail.com>
2020-06-02 14:14:22 -0400

    version 0.13.0 meta_cats, downsample, manual_cat
    This veresion adds support for setting caetegories via a meta_cat
    set of metadata dataframes. We also keep track of downsampled
    manual labeling of categories'

--- clustergrammer2/example.py
+++ clustergrammer2/example.py
@@ -28,6 +28,6 @@ class ExampleWidget(DOMWidget):
 
     mat_string = Unicode('ini_mat_string').tag(sync=True)
 
-    print('>> clustergrammer2 backend version 0.12.1')
+    print('>> clustergrammer2 backend version 0.13.0')
 
     network = Unicode('').tag(sync=True)
