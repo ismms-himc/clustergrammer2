9b2cd0eb3a190003e18f032e70dc27d541316a12
cornhundred <nickfloresfernandez@gmail.com>
2020-08-21 01:07:42 -0400

    version 0.16.0 new signature methods

--- clustergrammer2/_frontend.py
+++ clustergrammer2/_frontend.py
@@ -9,4 +9,4 @@ Information about the frontend package of the widgets.
 """
 
 module_name = "clustergrammer2"
-module_version = "0.15.2"
+module_version = "0.16.0"
--- clustergrammer2/_version.py
+++ clustergrammer2/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Nicolas Fernandez.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 15, 2)
+version_info = (0, 16, 0)
 __version__ = ".".join(map(str, version_info))
